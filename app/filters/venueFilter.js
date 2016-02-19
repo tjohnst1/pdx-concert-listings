eb98b902dd29a03ca2d6184a49c042c9bbaeb31f
Travis Johnston <tjohnst1@gmail.com>
2016-02-19 14:59:40 -0800

    apply venue filter to event listing

--- app/filters/venueFilter.js
+++ app/filters/venueFilter.js
@@ -2,7 +2,6 @@ import moment from 'moment';
 
 export default concertListings => {
   concertListings.filter('venueFilter', () => {
-    console.log('here')
     return function(events, venue){
       var filteredEvents = events.filter((event) => {
         return event.venue.name === venue;
