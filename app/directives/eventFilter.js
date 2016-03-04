3e768bbf34db4c211f492dc4802d01e0bb140d5c
Travis Johnston <tjohnst1@gmail.com>
2016-03-03 16:48:27 -0800

    add sliding effect for filter

--- app/directives/eventFilter.js
+++ app/directives/eventFilter.js
@@ -2,7 +2,7 @@ export default concertListings => {
   concertListings.directive('eventFilter', () => {
     return {
       restrict: 'E',
-      // controllerAs: 'eventFilterCtrl',
+      controllerAs: 'eventFilterCtrl',
       scope: {
         venues: '=',
         selectedVenue: '=',
@@ -11,17 +11,19 @@ export default concertListings => {
         itemsPerPage: '=',
         query: '='
       },
-      // bindToController: {
-      //   venues: '=',
-      //   selectedVenue: '=',
-      //   startingDate: '=',
-      //   endingDate: '=',
-      //   itemsPerPage: '='
-      // },
-      // controller: function($scope){
-      //   const eventFilterCtrl = this;
-      //   eventFilterCtrl.currentVal = 20;
-      // },
+      bindToController: {
+        venues: '=',
+        selectedVenue: '=',
+        startingDate: '=',
+        endingDate: '=',
+        itemsPerPage: '='
+      },
+      controller: function($scope){
+        const eventFilterCtrl = this;
+        eventFilterCtrl.toggleFilter = function(){
+          eventFilterCtrl.showFilter = !eventFilterCtrl.showFilter;
+        }
+      },
       template: require('../templates/eventFilter.html')
     }
   })
