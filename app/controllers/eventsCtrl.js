07bfe09177a4589fa52baf2df98f2975a243538e
Travis Johnston <tjohnst1@gmail.com>
2016-03-06 10:14:20 -0800

    add map settings to the events ctrl

--- app/controllers/eventsCtrl.js
+++ app/controllers/eventsCtrl.js
@@ -7,10 +7,6 @@ export default concertListings => {
     vm.filterOptions = {venue: "", startingDate: "", endingDate: "", itemsPerPage: "20", query: ""};
     vm.event = {};
 
-    // uiGmapGoogleMapApi.then(function(maps) {
-    //   vm.map = { center: { latitude: 45.5247002, longitude: -122.7142669 }, zoom: 12 };
-    // });
-
     EventsFactory.getData().on("value", function(snapshot) {
      const fbSnapshot = snapshot.val();
      let events = Object.keys(fbSnapshot).map(key => fbSnapshot[key]);
@@ -19,9 +15,6 @@ export default concertListings => {
         vm.venues = EventsFactory.getVenues(events);
         if ($stateParams.eventId){
           vm.event = EventsFactory.getEventById(vm.events, Number($stateParams.eventId));
-          // uiGmapIsReady.promise().then(function (maps) {
-          //   $scope.control.refresh();
-          // });
           uiGmapGoogleMapApi.then(function(maps) {
             vm.map = { center: { latitude: vm.event.venue.lat, longitude: vm.event.venue.lng }, zoom: 15 };
           });
