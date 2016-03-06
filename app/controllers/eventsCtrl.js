1aa2e1b953a49eae233d2f7cfcc80bdb250a03a5
Travis Johnston <tjohnst1@gmail.com>
2016-03-05 23:29:44 -0800

    switch out google maps directive

--- app/controllers/eventsCtrl.js
+++ app/controllers/eventsCtrl.js
@@ -1,11 +1,15 @@
 export default concertListings => {
-  concertListings.controller('EventsCtrl', ["EventsFactory", "$timeout", "$stateParams", function(EventsFactory, $timeout, $stateParams){
+  concertListings.controller('EventsCtrl', ["$scope", "EventsFactory", "$timeout", "$stateParams", "uiGmapGoogleMapApi", function($scope, EventsFactory, $timeout, $stateParams, uiGmapGoogleMapApi){
     var vm = this;
 
     vm.events = EventsFactory.events;
     vm.venues = EventsFactory.venues;
     vm.filterOptions = {venue: "", startingDate: "", endingDate: "", itemsPerPage: "20", query: ""};
     vm.event = {};
+
+    // uiGmapGoogleMapApi.then(function(maps) {
+    //   vm.map = { center: { latitude: 45.5247002, longitude: -122.7142669 }, zoom: 12 };
+    // });
 
     EventsFactory.getData().on("value", function(snapshot) {
      const fbSnapshot = snapshot.val();
@@ -15,6 +19,12 @@ export default concertListings => {
         vm.venues = EventsFactory.getVenues(events);
         if ($stateParams.eventId){
           vm.event = EventsFactory.getEventById(vm.events, Number($stateParams.eventId));
+          // uiGmapIsReady.promise().then(function (maps) {
+          //   $scope.control.refresh();
+          // });
+          uiGmapGoogleMapApi.then(function(maps) {
+            vm.map = { center: { latitude: vm.event.venue.lat, longitude: vm.event.venue.lng }, zoom: 15 };
+          });
         }
       });
     });
--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
+import 'lodash';
 import angular from 'angular';
 import angularfire from 'angularfire';
 import Firebase from 'firebase';
 import 'angular-ui-router';
-import 'angular-google-maps-native';
+// import 'angular-google-maps-native';
 import 'angular-utils-pagination';
+import 'angular-simple-logger';
+import 'angular-google-maps';
 
-const concertListings = angular.module('concertListings', ['firebase', 'ui.router', 'GoogleMapsNative', 'angularUtils.directives.dirPagination']);
+const concertListings = angular.module('concertListings', ['firebase', 'ui.router', 'nemLogging','uiGmapgoogle-maps', 'angularUtils.directives.dirPagination']);
+
+concertListings.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.23', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})
 
 require('./factories/eventsFactory')(concertListings);
 require('./controllers')(concertListings);
