c5e958716dfa7c8a189dd3bb7f4d51487a338fea
Travis Johnston <tjohnst1@gmail.com>
2016-03-05 22:28:05 -0800

    update styling; remove angular google maps

--- app/directives/index.js
+++ app/directives/index.js
@@ -2,4 +2,5 @@ export default concertListings => {
   require('./eventHeadline')(concertListings);
   require('./eventFilter')(concertListings);
   require('./sharingLinks')(concertListings);
+  require('./header')(concertListings);
 };
