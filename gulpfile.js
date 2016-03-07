4f6d28d779c106b9e404614e95be348d1a896a5d
Travis Johnston <tjohnst1@gmail.com>
2016-03-07 10:52:00 -0800

    update gulpfile to include publish and optimize img functions

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,17 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('publish', function(){
+  gulp.src('app/dist/**/*.*')
+  .pipe(gulp.dest('./'));
+})
+
+gulp.task('optimize-imgs', function(){
+  return gulp.src('./img/*')
+        .pipe(imagemin({ optimizationLevel: 5, progressive: true }))
+        .pipe(gulp.dest('./dist/images'));
+});
+
 gulp.task('watch', function(){
   gulp.watch('styles/**/*.scss', ['sass']);
 });
